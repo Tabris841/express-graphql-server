5dd0b3fd6042e233631003586dddb70e0dea6eb8
Anton Todorov <shadowtabris84@gmail.com>
2018-03-25 21:34:35 +0300

    fix for user resolvers

--- src/api/resources/user/user.resolvers.ts
+++ src/api/resources/user/user.resolvers.ts
@@ -1,18 +1,19 @@
 import { User } from './user.model';
 
-const getUser = (_, { id }) => User.findById(id).exec();
+const getUser = async (_, { id }) => await User.findById(id).exec();
 
-const allUsers = () => User.find({});
+const allUsers = async () => await User.find({}).exec();
 
-const createUser = (_, { input }) => User.create(input);
+const createUser = async (_, { input }) => await User.create(input);
 
-const updateUser = (_, { input }) => {
+const updateUser = async (_, { input }) => {
   const { id, ...update } = input;
 
-  return User.findByIdAndUpdate(id, update, { new: true }).exec();
+  return await User.findByIdAndUpdate(id, update, { new: true }).exec();
 };
 
-const deleteUser = (_, { id }) => User.findByIdAndRemove({ _id: id });
+const deleteUser = async (_, { id }) =>
+  await User.findByIdAndRemove({ _id: id });
 
 export const userResolvers = {
   Query: {
